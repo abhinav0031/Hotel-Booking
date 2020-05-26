c8cdd45153801dd1958236021e2331e2125169f6
abbrat <abhaysinghpat12@gmail.com>
2020-05-26 21:35:07 +0530

    added file processing to /hotel

--- server/Api/routes/hotel_router.js
+++ server/Api/routes/hotel_router.js
@@ -2,8 +2,27 @@ const express =require('express');
 const router=express.Router();
 const hotel=require('../models/hotel_model');
 const mongoose=require('mongoose');
-const multer=require('multer');
-const upload=multer({dest:'uploads/'});
+const multer  = require('multer')
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './upload/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, uniqueSuffix+ file.originalname)
+    }
+  })
+const fileFilter=(req,file,cb)=>{
+    if(file.mimetype=== 'image/jpeg'|| file.mimetype=== 'image/png'){
+        cb(null,true);
+    }else{
+        cb(null,false);
+    } 
+}
+const upload = multer({storage: storage , limits:{
+    fileSize:1024*1024*3
+}
+,fileFilter:fileFilter})
 
 router.get('/',(req,res,next)=>{
     const location=req.body.location;
@@ -14,7 +33,7 @@ router.get('/',(req,res,next)=>{
     })
 })
 
-router.post('/',upload.single('productImage'),(req,res,next)=>{
+router.post('/',upload.single('avatar'),(req,res,next)=>{
     console.log(req.file);
     const place=new hotel({
         _id:new mongoose.Types.ObjectId(),
@@ -23,7 +42,8 @@ router.post('/',upload.single('productImage'),(req,res,next)=>{
     location:req.body.location,
     price:req.body.price,
     contact:req.body.contact,
-    rooms:req.body.rooms
+    rooms:req.body.rooms,
+    img_url:req.file.path
     });
     place.save().then(result=>{
         res.status(200).json(result);
@@ -32,9 +52,10 @@ router.post('/',upload.single('productImage'),(req,res,next)=>{
     })
 })
 
-router.post('/img',upload.single('image'),(req,res)=>{
-    console.log(req.file)
-})
+/*router.post('/profile', upload.single('avatar'), function (req, res, next) {
+      console.log(req.files);
+      res.status(200).json({msg:"kaam hogaya"})
+  })*/
 
 module.exports=router;
 
