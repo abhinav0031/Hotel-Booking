722274b009b032cba8b9b5901d89b2f74ec20b3f
abbrat <abhaysinghpat12@gmail.com>
2020-06-08 00:46:05 +0530

    added number and date validation

--- server/Api/models/query_model.js
+++ server/Api/models/query_model.js
@@ -2,7 +2,10 @@ const mongoose=require('mongoose');
 const querySchema=mongoose.Schema({
    
     Name: {type:String,required:true},
-    phone: {type:String,required:true,min:10,max:10},
+    phone: {type:String,
+        required:true,
+       match:/^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/
+    },
     email:{type:String,
         required:true,
        match:/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
